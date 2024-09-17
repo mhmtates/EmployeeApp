8bfcb8f9cc259c536eb7203824a281812f8be1b8
Mehmet Ateş Özateş <ozatesatess@gmail.com>
2024-09-17 04:15:57 +0300

    Button added as a UI component and addworker.jsx file updated

---
+++ src/components/UI/Button.jsx
@@ -0,0 +1,11 @@
+
+
+const Button = (props) => {
+  return (
+    <button className={`p-2 text-lg bg-teal-700 text-white ${props.className}`}>
+    {props.children}
+    </button>
+  )
+}
+
+export default Button
--- src/components/Workers/AddWorker.jsx
+++ src/components/Workers/AddWorker.jsx
@@ -1,8 +1,28 @@
+import Button from "../UI/Button";
 import Card from "../UI/Card";
+import { useState } from "react";
+
 const AddWorker = () => {
+  
+   const [workerName, setWorkerName] = useState("");
+   const [salary, setSalary] = useState("");
+
+   const workerNameChangeHandler = (e) => {
+     setWorkerName(e.target.value);
+   }
+   const salaryChangeHandler = (e) => {
+     setSalary(e.target.value);
+  }
+  const addWorkerHandler = (e) => {
+      e.preventDefault();
+      setWorkerName("");
+      setSalary("");
+      console.log(workerName,salary);
+  }
+
   return (
     <Card className="mt-10">
-      <form className="flex flex-col gap-y-2">
+      <form className="flex flex-col gap-y-2" onSubmit={addWorkerHandler}>
         <label htmlFor="name" className="font-medium">
           Çalışan ismi
         </label>
@@ -11,6 +31,8 @@ const AddWorker = () => {
           type="text"
           placeholder="Çalışan ismi yazınız."
           id="name"
+          onChange={workerNameChangeHandler}
+          value={workerName}
         />
         <label htmlFor="salary" className="font-medium">
           Maaş miktarı
@@ -20,10 +42,10 @@ const AddWorker = () => {
           className="max-w-[40rem] w-full mx-auto border p-2"
           placeholder="Maaş miktarı yazınız."
           id="salary"
+          onChange={salaryChangeHandler}
+          value={salary}
         />
-        <button className="p-2 text-lg bg-teal-700 text-white mt-2">
-          Ekle
-        </button>
+        <Button className="mt-2">Ekle</Button>
       </form>
     </Card>
   );
